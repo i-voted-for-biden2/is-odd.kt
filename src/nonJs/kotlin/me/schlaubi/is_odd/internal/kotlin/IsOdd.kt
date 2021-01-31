f41de8cee705b9ee143702ed92529a34949cca26
Michael Rittmeister <michael@rittmeister.in>
2021-01-31 01:00:10 +0100

    Fix safe value thing

--- src/nonJs/kotlin/me/schlaubi/is_odd/internal/kotlin/IsOdd.kt
+++ src/nonJs/kotlin/me/schlaubi/is_odd/internal/kotlin/IsOdd.kt
@@ -4,7 +4,13 @@ class TypeError(message: String) : Throwable(message)
 
 internal fun isOdd(input: Any?): Boolean {
     return when (input) {
-        is Long -> (input and 1) == 1L
+        is Long -> {
+            if (input !in -9007199254740991L..9007199254740991L) {
+                throw Error("value exceeds maximum safe integer")
+            }
+
+            (input and 1) == 1L
+        }
         is Int -> (input and 1) == 1
         !is Number -> throw TypeError("Expected an integer")
         else -> throw Error("Exceeds max integer")
