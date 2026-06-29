2d199b4e46c87f0de75e0700d8139d6327caf105
skloobi <michael@rittmeister.in>
2026-06-29 21:53:05 +0200

    Update dependencies - Update Gradle - Update Kotlin - Support more native targets

--- docs/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":dokkaHtml/commonMain": [], ":dokkaHtml/jsMain": [":dokkaHtml/commonMain"], ":dokkaHtml/jvmMain": [":dokkaHtml/commonMain",":dokkaHtml/nonJs"], ":dokkaHtml/nativeMain": [":dokkaHtml/commonMain",":dokkaHtml/nonJs"], ":dokkaHtml/nonJs": []}'
